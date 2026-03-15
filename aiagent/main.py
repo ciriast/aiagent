ce3e13b4df8a3bd99204a364919af8e0b2de9ce8
carlos <ciriastorres@gmail.com>
2026-03-15 10:34:14 -0600

    working with argparse

--- aiagent/main.py
+++ aiagent/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+if api_key == None:
+    raise RuntimeError("The api key is not available")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+client = genai.Client(api_key=api_key)
+
+my_response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+if my_response.usage_metadata is not None:
+    print(f'Prompt tokens: {my_response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {my_response.usage_metadata.candidates_token_count}')
+else:
+    raise RuntimeError("No usage_metadata object in the response")
+
+print(my_response.text)
